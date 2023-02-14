bc89254e48508ff462872e0836edb112c650fa7a
unknown <tombirbeck47@gmail.com>
2023-02-14 17:35:41 +0000

    bar sized now dynamically generated, total amounts shown but need to set to only show on hover

---
+++ main.js
@@ -0,0 +1,20 @@
+const chart = document.querySelector('.chart')
+const bars = document.querySelectorAll('.chart-bar')
+const amount = document.querySelectorAll('.chart-bar-total')
+const res = await fetch ('./data.json')
+const data = await res.json()
+const date = new Date().getDay()
+
+
+const createBars = (item) => {
+    for (let i = 0; i < data.length; i++) {
+        const height = Math.round((item[i].amount/100) * 12)
+        if (date === i + 1){
+            bars[i].classList.add('today')
+        }
+        bars[i].style.height = `${height}rem`
+        amount[i].textContent = '$'+ data[i].amount
+   }
+}
+
+createBars(data)
